05bd8b912c11dc0adf1ec46eb6d815ecee92b49e
hiroshi <hiroshi.tan57@gmail.com>
2024-12-06 14:32:56 -0500

    refactor: system monitor with all of contracts

--- src/system.service.ts
+++ src/system.service.ts
@@ -3,13 +3,17 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 import { ContractMonitorService } from './contract-monitor.service';
 import { TasksService } from './tasks/tasks.service';
 import { TradingVariableService } from './global/trading-variable.service';
+import { BotsService } from './bots/bots.service';
+import { PnlSnapshotsService } from './trade-histories/pnlsnapshot.service';
 
 @Injectable()
 export class SystemService {
   constructor(
     private contractMonitorService: ContractMonitorService,
     private tasksService: TasksService,
+    private pnlSnapshotService: PnlSnapshotsService,
     private tradingVariableService: TradingVariableService,
+    private botsService: BotsService,
   ) {}
 
   @Cron(CronExpression.EVERY_SECOND)
@@ -19,8 +23,25 @@ export class SystemService {
       this.contractMonitorService.status === 'ready' &&
       this.tradingVariableService.status === 'ready'
     ) {
-      // await this.contractMonitorService.checkContract(1);
-      // await this.tasksService.performAvailableTasks();
+      await this.contractMonitorService.checkContracts();
+      await this.tasksService.performAvailableTasks();
+    }
+  }
+
+  @Cron(CronExpression.EVERY_MINUTE)
+  async executeCronForBots() {
+    if (
+      this.tradingVariableService.status === 'ready' &&
+      this.botsService.status === 'ready'
+    ) {
+      await this.botsService.checkAndUpdateAllBots();
+    }
+  }
+
+  @Cron(CronExpression.EVERY_MINUTE)
+  async executeCronForSnapshot() {
+    if (this.pnlSnapshotService.status === 'ready') {
+      await this.pnlSnapshotService.dayUpdate();
     }
   }
 }
