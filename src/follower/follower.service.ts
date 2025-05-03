6ca5d2489e0871a84f2f7766cb8f77cd09fa9d62
takeshi <takeshisuz57@gmail.com>
2025-05-02 21:28:22 -0400

    update cron schedule

--- src/follower/follower.service.ts
+++ src/follower/follower.service.ts
@@ -1081,7 +1081,7 @@ export class FollowerService {
       const usdcMap: Record<string, bigint> = {};
       const pnlSnapshotsMap: Record<string, PnlSnapshot[]> = {};
 
-      for (const entity of followerEntities) {
+      const promises = followerEntities.map(async (entity) => {
         const usdcBalance = await publicClient.readContract({
           address: collateralInfo.collateral,
           abi: erc20Abi,
@@ -1104,7 +1104,9 @@ export class FollowerService {
           );
 
         pnlSnapshotsMap[entity.address] = pnlSnapshots;
-      }
+      });
+
+      await Promise.allSettled(promises);
 
       return followerEntities.map((entity) => ({
         ...entity,
