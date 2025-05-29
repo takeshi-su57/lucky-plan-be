3444a6a0b4ee9a81851bc2aaea819d1d93229e7f
takeshi <takeshisuz57@gmail.com>
2025-05-29 14:15:58 -0400

    chore: fix issue

--- src/plans/autoplansV2.service.ts
+++ src/plans/autoplansV2.service.ts
@@ -372,7 +372,10 @@ export class AutoPlansV2Service {
         title: 'Auto Plan V2',
         description: 'This is an auto plan',
         scheduledStart: new Date(),
-        scheduledEnd: dayjs(new Date()).add(3, 'hours').add(30, 'min').toDate(),
+        scheduledEnd: dayjs(new Date())
+          .add(3, 'hours')
+          .add(30, 'minutes')
+          .toDate(),
       };
 
       const plan = await this.planService.create(
